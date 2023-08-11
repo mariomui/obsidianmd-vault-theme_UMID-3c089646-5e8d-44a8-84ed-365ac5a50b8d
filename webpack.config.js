8d9f5627044115cbe305b67a57a1d0556ae3bad8
mariomui <yi.kan.mui@gmail.com>
2023-08-11 01:51:27 -0700

    Really bad hack to get scss to to bind.

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,63 @@
-// Generated using webpack-cli https://github.com/webpack/webpack-cli
+// // Generated using webpack-cli https://github.com/webpack/webpack-cli
 const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const isProduction = process.env.NODE_ENV == "production";
 
 // const stylesHandler = isProduction ? MiniCssExtractPlugin.loader : 'style-loader';
-const minicss = new MiniCssExtractPlugin({
-  filename: "[name].css",
-  chunkFilename: "[name].css",
-});
+const minicss = new MiniCssExtractPlugin();
 
 /**
  * @type { import('webpack').Configuration }
  */
 const config = {
-  entry: [__dirname + "/src/index.scss"],
+  entry: {
+    index: path.resolve(__dirname, "src", "index.scss"),
+    importtest: path.resolve(__dirname, "src", "importtest.scss"),
+  },
   output: {
-    publicPath: "dist",
-    cssFilename: "[name].css",
-    // filename: (a,b) => {
-    //     return false
-    // },
+    filename: (filename) => {
+      return false;
+    },
     clean: true,
     path: path.resolve(__dirname, "dist"),
   },
   plugins: [minicss],
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        js: {
+          type: "js",
+          chunks: (chunk) => {
+            console.log(chunk, "me");
+            return false;
+          },
+        },
+        index: {
+          name: "index",
+          type: "css/mini-extract",
+          chunks: (chunk) => {
+            if (chunk.name === "index") {
+              return chunk;
+            }
+            return chunk.name === "index";
+          },
+          enforce: true,
+        },
+        importtest: {
+          name: "importtest",
+          type: "css/mini-extract",
+          chunks: (chunk) => {
+            if (chunk.name === "importtest") {
+              return chunk;
+            }
+            return chunk.name === "importtest";
+          },
+          enforce: true,
+        },
+      },
+    },
+  },
   module: {
     rules: [
       {
@@ -35,26 +68,32 @@ const config = {
           MiniCssExtractPlugin.loader,
           {
             loader: "css-loader",
-            options: {
-              modules: true,
-              sourceMap: true,
-              importLoader: 2,
-            },
+            // options: {
+            //   modules: {
+            //     mode: "local",
+            //     exportGlobals: true,
+            //     localIdentName: "[path][name]__[local]--[hash:base64:5]",
+            //     localIdentContext: path.resolve(__dirname, "src"),
+            //     localIdentHashSalt: "my-custom-hash",
+            //     namedExport: true,
+            //     exportLocalsConvention: "camelCase",
+            //     exportOnlyLocals: false,
+            //   },
+            // },
           },
-          ,
           "postcss-loader",
           "sass-loader",
         ],
       },
-      // {
-      //     test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-      //     type: 'asset',
-      // },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: "asset",
+      },
     ],
   },
-  // resolve: {
-  //     extensions: ['.scss'],
-  // },
+  resolve: {
+    extensions: [".scss"],
+  },
 };
 
 module.exports = () => {
