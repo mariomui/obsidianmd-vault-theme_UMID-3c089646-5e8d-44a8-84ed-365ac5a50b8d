01a3ea3808c856229c6c80faa9c6170cf23b2733
mariomui <yi.kan.mui@gmail.com>
2023-08-13 07:26:39 -0700

    Create working prototype of dev system

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,22 @@
-// // Generated using webpack-cli https://github.com/webpack/webpack-cli
 const path = require("path");
-// const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const webpack = require("webpack");
 const middleware = require("webpack-dev-middleware");
-
-const isProduction = process.env.NODE_ENV == "production";
-
-// const stylesHandler = isProduction ? MiniCssExtractPlugin.loader : 'style-loader';
-// const minicss = new MiniCssExtractPlugin();
-
+require("dotenv").config();
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const isProduction = process.env.NODE_ENV === "production";
+const isDev = process.env.NODE_ENV !== "production";
+const { access, constants } = require("fs/promises");
+const DEV_WRITE_PATH = process.env.OMD_SNIPPETS_PATH;
+const util = require("util")
 const HtmlBundlerPlugin = require("html-bundler-webpack-plugin");
 
-const plugins = [
-  new HtmlBundlerPlugin({
-    // js: {
-    //   // define the output name of a generated JS file here
-    //   filename: "assets/js/[name].[contenthash:8].js",
-    // },
-    outputPath: path.resolve(__dirname, "graveyard"),
-    entry: {
-      // define templates here
-      index: {
-        // => dist/index.html (key is output filename w/o '.html')
-        import: "src/index.html", // template file
-        data: { title: "Homepage", name: "Heisenberg" }, // pass variables into template
-      },
-    },
-    css: {
-      // define the output name of a generated CSS file here
-      filename: "[name].css",
-    },
-  }),
-];
+
 /**
  * @type { import('webpack').Configuration }
  */
 const config = {
-  output: {
-    clean: true,
-  },
-  plugins,
+  plugins: [],
+  mode: "development",
   module: {
     rules: [
       {
@@ -61,13 +38,15 @@ const config = {
     ],
   },
   resolve: {
-    extensions: [".js",".scss"],
+    extensions: [".js", ".scss"],
   },
   devServer: {
     devMiddleware: {
       writeToDisk: (filePath) => {
+        console.log({ filePath });
         return /.css$/.test(filePath);
-      },    },
+      },
+    },
     static: path.join(__dirname, "dist"),
     port: 9100,
     watchFiles: {
@@ -79,13 +58,100 @@ const config = {
   },
 };
 
-module.exports = () => {
+const logg = createLogg();
+module.exports = async () => {
   if (isProduction) {
+    config.plugins.push(configureHtmlBundlerPluginForProd());
     config.mode = "production";
-
     config.plugins.push(new MiniCssExtractPlugin());
-  } else {
-    config.mode = "development";
+    return config;
   }
+
+  if (isDev) {
+    try {
+      await access(DEV_WRITE_PATH, constants.F_OK);
+    } catch (err) {
+      console.log({ err });
+      return process.exit(1);
+    }
+    // never use cleanwebpackplugin in dev mode.
+    // does devServer have priority?
+    const reference = {
+      output: {
+        x: 1
+      }
+    }
+    setConfig(["output.path", DEV_WRITE_PATH], config)
+
+    // console.log(util.inspect(preppedConfig, { depth: 4, colors: true }))
+
+    config.plugins.push(configureHtmlBundlerPluginForDev());
+
+    // config.devServer.static = DEV_WRITE_PATH;
+    return config;
+  }
+
   return config;
 };
+
+
+// helpers
+
+function createLogg(config = {}) {
+  const _config = {
+    depth: 3,
+    color: true,
+    ...config
+  }
+  return function logg(obj) {
+    const logLine = util.inspect(obj, _config);
+    console.log(logLine)
+  }
+}
+function setConfig(tuple, config) {
+  const [location, value] = tuple
+  const locations = location.split(".");
+  let context = config;
+  let previousContext = null;
+  let lastLocation = locations.at(0);
+  for (let location of locations) {
+    previousContext = context;
+    if (context?.[location]) {
+      context = context[location]
+      lastLocation = location;
+      continue;
+    }
+    context[location] = {};
+    context = context[location]
+    lastLocation = location;
+  }
+  previousContext[lastLocation] = value;
+  return previousContext;
+}
+function configureHtmlBundlerPluginForDev() {
+  return new HtmlBundlerPlugin({
+    outputPath: path.resolve(__dirname, "graveyard"),
+    entry: {
+      index: {
+        import: "src/index.html",
+      },
+    },
+    css: {
+      filename: "[name].css",
+    },
+  });
+}
+function configureHtmlBundlerPluginForProd() {
+  return new HtmlBundlerPlugin({
+    // - fix: when the Webpack `output.path` option is undefined, set the default path as CWD + `/dist`
+    outputPath: path.resolve(__dirname, "graveyard"),
+    entry: {
+      index: {
+        import: "src/index.html",
+      },
+    },
+    css: {
+      filename: "[name].css",
+    },
+  });
+}
