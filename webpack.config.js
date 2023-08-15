e9cdf81c148293be5bdecc73c02b2b6f044b3dbe
mariomui <yi.kan.mui@gmail.com>
2023-08-15 13:06:46 -0700

    auto

--- webpack.config.js
+++ webpack.config.js
@@ -5,11 +5,11 @@ const path = require("path");
 const webpack = require("webpack");
 const middleware = require("webpack-dev-middleware");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { access, constants, readdir } = require("fs/promises");
-const util = require("util")
+const util = require("util");
 const HtmlBundlerPlugin = require("html-bundler-webpack-plugin");
-const http = require('node:http');
+const http = require("node:http");
 // derived toolboxes
 const logg = createLogg();
 
@@ -35,7 +35,8 @@ const config = {
         test: /\.(css|sass|scss)$/,
         // https://webpack.js.org/configuration/module/#ruleuse
         exclude: /node_modules/,
-        use: [ // RTL
+        use: [
+          // RTL
           {
             loader: "css-loader",
           },
@@ -51,12 +52,11 @@ const config = {
     ],
   },
   resolve: {
-    extensions: [".scss"],
+    extensions: [".js", ".scss"],
   },
   devServer: {
     devMiddleware: {
       writeToDisk: (filePath) => {
-
         return /.css$/.test(filePath);
       },
     },
@@ -72,22 +72,19 @@ const config = {
 };
 
 module.exports = async () => {
-
-
-
-  logg({ isProduction, isDev })
+  logg({ isProduction, isDev });
   if (isProduction) {
     config.mode = "production";
-    setConfig(["output.path", PROD_WRITE_PATH], config)
+    setConfig(["output.path", PROD_WRITE_PATH], config);
     config.plugins.push(new CleanWebpackPlugin({ dry: true }));
     config.plugins.push(configureHtmlBundlerPluginForProd());
     return config;
   }
 
   if (isDev) {
-    const { data: _port, err } = await genPort(port)
+    const { data: _port, err } = await genPort(port);
     const used = process.memoryUsage().heapUsed / 1024 / 1024;
-    logg({ used })
+    logg({ used });
 
     if (err) {
       logg(err);
@@ -99,8 +96,8 @@ module.exports = async () => {
     } catch (err) {
       return process.exit(1);
     }
-    setConfig(["devServer.port", _port], config)
-    setConfig(["output.path", DEV_WRITE_PATH], config)
+    setConfig(["devServer.port", _port], config);
+    setConfig(["output.path", DEV_WRITE_PATH], config);
     config.plugins.push(new CleanWebpackPlugin({ dry: true }));
     config.plugins.push(configureHtmlBundlerPluginForDev());
 
@@ -110,7 +107,6 @@ module.exports = async () => {
   return config;
 };
 
-
 // # helpers
 
 // ## util
@@ -118,17 +114,17 @@ function createLogg(config = {}) {
   const _config = {
     depth: 3,
     color: true,
-    ...config
-  }
+    ...config,
+  };
   return function logg(obj) {
     const logLine = util.inspect(obj, _config);
-    console.error(logLine)
-  }
+    console.error(logLine);
+  };
 }
 
 // ## webpack
 function setConfig(tuple, config) {
-  const [location, value] = tuple
+  const [location, value] = tuple;
   const locations = location.split(".");
   let context = config;
   let previousContext = null;
@@ -136,12 +132,12 @@ function setConfig(tuple, config) {
   for (let location of locations) {
     previousContext = context;
     if (context?.[location]) {
-      context = context[location]
+      context = context[location];
       lastLocation = location;
       continue;
     }
     context[location] = {};
-    context = context[location]
+    context = context[location];
     lastLocation = location;
   }
   previousContext[lastLocation] = value;
@@ -189,31 +185,22 @@ async function genPort(port, recursionLimit = 5) {
 }
 async function genCheckPortAvailability(HTTPserver, port, recursionLimit = 5) {
   return new Promise((resolve) => {
-
-    HTTPserver
-      .listen(port, function () {
-        logg(`Port ${port} is now opening...`)
-        resolve({ data: port, err: null })
-        HTTPserver.close()
-      })
-      .on('error', function (err) {
-        if (err.code === 'EADDRINUSE') {
-          logg('Address in use, retrying on port ' + ++port);
-          setTimeout(function () {
-            if (--recursionLimit > 0) {
-              HTTPserver.listen(port);
-            } else {
-              HTTPserver.close();
-              resolve({ data: port, err: new Error("port not found") })
-            }
-          })
-        }
-      })
+    HTTPserver.listen(port, function () {
+      logg(`Port ${port} is now opening...`);
+      resolve({ data: port, err: null });
+      HTTPserver.close();
+    }).on("error", function (err) {
+      if (err.code === "EADDRINUSE") {
+        logg("Address in use, retrying on port " + ++port);
+        setTimeout(function () {
+          if (--recursionLimit > 0) {
+            HTTPserver.listen(port);
+          } else {
+            HTTPserver.close();
+            resolve({ data: port, err: new Error("port not found") });
+          }
+        });
+      }
+    });
   });
 }
-
-
-
-
-
-
